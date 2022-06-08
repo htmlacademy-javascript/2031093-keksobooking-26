49c3574d97351322f889921fc601636a072cf99a
VitalSTU <kolysvit@mail.ru>
2022-06-08 12:06:42 +0300

    Function declarations changed to arrow functions. Commented code cleaned

--- js/main.js
+++ js/main.js
@@ -1,21 +1,16 @@
-function randomizerParametersAreCorrect(min, max, functionErrorString) {
-  //let result = true;
-
+const randomizerParametersAreCorrect = (min, max, functionErrorString) => {
   if (max === undefined) {
     throw new Error(`${functionErrorString} There are must be two number parameters.`);
-    //result = false;
   } else if (!Number.isFinite(min) || !Number.isFinite(max) || min < 0 || max < 0) {
     throw new Error(`${functionErrorString}  Parameters "min" and "max" must be numbers greater or equal to zero.`);
-    //result = false;
   } else if (min > max || min === max) {
     throw new Error(`${functionErrorString} Parameter "min" must be less then parameter "max".`);
-    //result = false;
   }
 
   return true;
-}
+};
 
-function getRandomNumber(min, max, tail, functionError) {
+const getRandomNumber = (min, max, tail, functionError) => {
   const pow = Math.pow(10, tail);
   let result = null;
 
@@ -26,19 +21,19 @@ function getRandomNumber(min, max, tail, functionError) {
   }
 
   return result;
-}
+};
 
-function getRandomIntegerNumber(min, max) {
+const getRandomIntegerNumber = (min, max) => {
   const functionError = 'getRandomInteger function error!';
 
   return getRandomNumber(min, max, 0, functionError);
-}
+};
 
-function getRandomRealNumber(min, max, tail) {
+const getRandomRealNumber = (min, max, tail) => {
   const functionError = 'getRandomRealNumber function error!';
 
   return getRandomNumber(min, max, tail, functionError);
-}
+};
 
 getRandomRealNumber(77.7, 888.8, 4);
 getRandomIntegerNumber(77, 77);
