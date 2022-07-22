12c6209f78cbcc2ce61dcc64c29c05c9e9250862
VitalSTU <kolysvit@mail.ru>
2022-07-22 07:21:14 +0300

    Backup. Almost all filters dine. Filter by features to do.

--- js/map.js
+++ js/map.js
@@ -1,8 +1,8 @@
 import {
-  setAdFormToActive
-  , setFilterFormToActive
-  , setAddress
-  , formAdElement
+  setAdFormToActive,
+  setFilterFormToActive,
+  setAddress,
+  formAdElement
 } from './form.js';
 
 import {
@@ -14,17 +14,40 @@ import {
 } from './popup.js';
 
 import {
-  getRandomIntegerNumber
-  , showMapPinsAlert
+  getRandomIntegerNumber,
+  showMapPinsAlert
 } from './utils.js';
 
 const DECIMAL_MANTISSA_LENGTH = 5;
 const INITIAL_MAP_SCALE = 16;
 const MAX_ADS_QUANTITY = 10;
+const ANY = 'any';
 const centerOfTokyoCoordinates = {
   lat: 35.69771374623864,
   lng: 139.7730618400656,
 };
+const priceFilterOption = {
+  'any': [Number.NEGATIVE_INFINITY, Number.POSITIVE_INFINITY],
+  'middle': [10000, 50000],
+  'low': [Number.NEGATIVE_INFINITY, 10000],
+  'high': [50000, Number.POSITIVE_INFINITY],
+};
+// const featureOption = {
+//   wifi,
+//   'dishwasher': ,
+//   parking,  //'parking', 'wifi', 'washer', 'dishwasher', 'conditioner', 'elevator'
+//   'washer': ,
+//   'elevator': ,
+//   'conditioner': ,
+// };
+
+
+const formFiltersElement = document.querySelector('.map__filters');
+const housingTypeElement = document.querySelector('#housing-type');
+const housingPriceElement = document.querySelector('#housing-price');
+const housingRoomsElement = document.querySelector('#housing-rooms');
+const housingGuestsElement = document.querySelector('#housing-guests');
+const housingFeaturesElement = document.querySelector('#housing-features');
 
 const map = L.map('map-canvas');
 
@@ -61,14 +84,74 @@ const createMarker = (ad) => {
     .bindPopup(getPopup(ad));
 };
 
-const setSimilarAdsToMap = (similarAds) => {
-  const firstAdNumber = getRandomIntegerNumber(0, similarAds.length - MAX_ADS_QUANTITY);
+const renderAds = async (ads) => {
+  map.closePopup();
+  markerGroup.clearLayers();
 
-  similarAds
-    // .slice(firstAdNumber, firstAdNumber + MAX_ADS_QUANTITY)
+  const firstAdNumber = (ads.length <= MAX_ADS_QUANTITY) ? 0 : getRandomIntegerNumber(0, ads.length - MAX_ADS_QUANTITY);
+
+  ads
+    .slice(firstAdNumber, firstAdNumber + MAX_ADS_QUANTITY)
     .forEach((ad) => {
       createMarker(ad);
     });
+};
+
+const isTypeMatchesToFilter = ({offer: {type}}, {value: filter}) => type && (filter === ANY || type === filter);
+
+const isPriceMatchesToFilter = ({offer: {price}}, {value: filter}) => price &&
+  (filter === ANY || price >= priceFilterOption[filter][0] && price < priceFilterOption[filter][1]);
+
+const isRoomsMatchesToFilter = ({offer: {rooms}}, {value: filter}) => rooms && (filter === ANY || rooms === +filter);
+
+const isGuestsMatchesToFilter = ({offer: {guests}}, {value: filter}) => (guests || !(+guests)) &&
+  (filter === ANY || guests === +filter);
+
+
+
+
+const setTypeFilterEvents = (similarAds) => {
+  const filterFeaturesValue = [...housingFeaturesElement.querySelectorAll('[name="features"]')]
+    .filter((item) => item.checked).map((item) => item.value);
+
+  // console.log(similarAds.filter((ad) => ad.offer.features && ad.offer.features.length === 6));
+  // console.log(similarAds.filter((ad) => !(ad.offer.features)));
+  console.log(filterFeaturesValue.length + ' - ' + filterFeaturesValue);
+
+  const typeMatches = (ad) => isTypeMatchesToFilter(ad, housingTypeElement);
+  const priceMatches = (ad) => isPriceMatchesToFilter(ad, housingPriceElement);
+  const roomsMatches = (ad) => isRoomsMatchesToFilter(ad, housingRoomsElement);
+  const guestsMatches = (ad) => isGuestsMatchesToFilter(ad, housingGuestsElement);
+
+  const featuresMatches = (ad) => !(filterFeaturesValue.length) || ad.offer.features &&
+    ad.offer.features.map((f) => filterFeaturesValue.includes(f)).reduce((f1, f2) => f1 && f2, true);
+
+  let sortedAds = similarAds
+    .filter(typeMatches)
+    .filter(priceMatches)
+    .filter(roomsMatches)
+    .filter(guestsMatches)
+    // .filter(featuresMatches)
+    ;
+  console.log(sortedAds);
+
+  sortedAds = sortedAds.filter((ad) => {
+    console.log(featuresMatches(ad));
+    return featuresMatches(ad)
+  });
+  console.log(sortedAds);
+
+  renderAds(sortedAds);
+};
+
+const setMapFilterEvents = async (similarAds) => {
+  formFiltersElement.addEventListener('change', () => setTypeFilterEvents(similarAds));
+};
+
+const setSimilarAdsToMap = async (similarAds) => {
+
+  await renderAds(similarAds);
+  await setMapFilterEvents(similarAds);
 
   setFilterFormToActive();
 };
@@ -101,7 +184,19 @@ formAdElement.addEventListener('reset', () => {
   }, 100);
 });
 
-tileLayer.addTo(map);
-mainPinMarker.addTo(map);
-markerGroup.addTo(map);
-getData(setSimilarAdsToMap, showMapPinsAlert);
+const renderMap = (callback) => {
+  try {
+
+    tileLayer.addTo(map);
+    mainPinMarker.addTo(map);
+    markerGroup.addTo(map);
+
+    callback();
+
+  } catch (error) {
+    throw new Error(`${error.name}: ${error.message},
+${error.stack}`);
+  }
+};
+
+renderMap(() => {getData(setSimilarAdsToMap, showMapPinsAlert);});
